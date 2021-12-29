855ad93ff12225b4917a985c36000d81a145d71a
Enrique Arias <emoriarty81@gmail.com>
2021-12-29 19:43:55 +0100

    test unsupported block

--- test/notion_to_md/converter_spec.rb
+++ test/notion_to_md/converter_spec.rb
@@ -14,32 +14,41 @@ fixture = YAML.load_file("#{Dir.pwd}/test/fixtures/notion/block_children.yml")
 NOTION_PAGE_ID = 'notion_page_id'
 
 describe NotionToMd::Converter do
-  before do
-    @block_children = NotionOutput.new(JSON.parse(fixture['output']))
-    @notion = MiniTest::Mock.new
-    @notion.expect(:block_children, @block_children, [{ id: NOTION_PAGE_ID }])
-  end
-
-  describe 'when bold annotations' do
-    it 'wraps with **' do
-      @block_children[:results].each do |block|
-        block[:paragraph][:text].each { |txt| txt[:annotations][:bold] = true }
+  describe 'when paragraph' do
+    describe 'with bold annotation' do
+      before do
+        @block_children = NotionOutput.new(JSON.parse(fixture['paragraph']))
+        @block_children[:results].each do |block|
+          block[:paragraph][:text].each { |txt| txt[:annotations][:bold] = true }
+        end
+        @notion = MiniTest::Mock.new
+        @notion.expect(:block_children, @block_children, [{ id: NOTION_PAGE_ID }])
       end
 
-      Notion::Client.stub :new, @notion do
-        output = NotionToMd::Converter.new(page_id: NOTION_PAGE_ID).convert
-        output.split('\n\n').each do |line|
-          assert line.start_with?('**')
-          assert line.end_with?('**')
+      it 'wraps with **' do
+        Notion::Client.stub :new, @notion do
+          output = NotionToMd::Converter.new(page_id: NOTION_PAGE_ID).convert
+          output.split('\n\n').each do |line|
+            assert line.start_with?('**')
+            assert line.end_with?('**')
+          end
         end
       end
     end
   end
 
-  it 'converts to markdown' do
-    Notion::Client.stub :new, @notion do
-      converter = NotionToMd::Converter.new(page_id: NOTION_PAGE_ID)
-      output = converter.convert
+  describe 'when unsupported block type' do
+    before do
+      @block_children = NotionOutput.new(JSON.parse(fixture['file']))
+      @notion = MiniTest::Mock.new
+      @notion.expect(:block_children, @block_children, [{ id: NOTION_PAGE_ID }])
+    end
+
+    it 'returns nothing' do
+      Notion::Client.stub :new, @notion do
+        output = NotionToMd::Converter.new(page_id: NOTION_PAGE_ID).convert
+        assert output.empty?
+      end
     end
   end
 end
