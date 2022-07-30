977615a419686ef6944e4227f339e17adf0e7013
Enrique Arias <enrique.arias.cervero@gmail.com>
2022-07-30 08:23:31 +0200

    (chore) ruby 3.1 support (#23)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,19 @@ require File.expand_path('../lib/notion_to_md', __dir__)
 
 NotionToMd::Logger.level = Logger::ERROR
 
-NOTION_PAGE = YAML.load_file(File.expand_path('fixtures/notion_page.yml', __dir__))
-NOTION_BLOCK_CHILDREN = YAML.load_file(File.expand_path('fixtures/notion_block_children.yml', __dir__))
+if RUBY_VERSION.start_with?('3.1')
+  NOTION_PAGE = YAML.load_file(
+    File.expand_path('fixtures/notion_page.yml', __dir__),
+    permitted_classes: [Hashie::Array, Notion::Messages::Message]
+  )
+  NOTION_BLOCK_CHILDREN = YAML.load_file(
+    File.expand_path('fixtures/notion_block_children.yml', __dir__),
+    permitted_classes: [Hashie::Array, Notion::Messages::Message]
+  )
+else
+  NOTION_PAGE = YAML.load_file(File.expand_path('fixtures/notion_page.yml', __dir__))
+  NOTION_BLOCK_CHILDREN = YAML.load_file(File.expand_path('fixtures/notion_block_children.yml', __dir__))
+end
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
