380c39c7a9059d0dbcf7fec126f3cca599180556
Enrique Arias <emoriarty81@gmail.com>
2022-09-19 18:55:19 +0200

    wip: table header

--- lib/notion_to_md/blocks/table_block.rb
+++ lib/notion_to_md/blocks/table_block.rb
@@ -4,7 +4,25 @@ module NotionToMd
   module Blocks
     class TableBlock < Block
       def to_md
-        markdownify_children(0).join("\n")
+        table = markdownify_children(0)
+
+        table_header = table[0]
+        table_aligment = markdownify_aligment
+        table_body = table[1..table.size]
+
+        [table_header, table_aligment, table_body.join("\n")].compact.join("\n")
+      end
+
+      private
+
+      def row_size
+        @row_size ||= children[0].block.table_row.cells.size
+      end
+
+      def markdownify_aligment
+        if block.table.has_column_header
+          "|#{row_size.times.map { '---' }.join('|')}|"
+        end
       end
     end
   end
