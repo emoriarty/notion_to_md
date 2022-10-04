3c3fdab3df6183b0c22e4d17b8a70bb7db8ed784
Enrique Arias <emoriarty81@gmail.com>
2022-10-04 21:58:32 +0200

    test permitted block

--- spec/notion_to_md/blocks_spec.rb
+++ spec/notion_to_md/blocks_spec.rb
@@ -11,13 +11,13 @@ describe(NotionToMd::Blocks) do
     end
 
     context('when has children and is not permitted') do
-      let(:block_notion) { Hashie::Mash.new(has_children: true, type: 'paragraph') }
+      let(:block_notion) { Hashie::Mash.new(has_children: true, type: 'not_permitted_block') }
 
       it { expect(described_class.permitted_children?(block: block_notion)).to be(false) }
     end
 
     context('when has no children and is not permitted') do
-      let(:block_notion) { Hashie::Mash.new(has_children: false, type: 'paragraph') }
+      let(:block_notion) { Hashie::Mash.new(has_children: false, type: 'not_permitted_block') }
 
       it { expect(described_class.permitted_children?(block: block_notion)).to be(false) }
     end
@@ -26,6 +26,13 @@ describe(NotionToMd::Blocks) do
       let(:block_notion) { Hashie::Mash.new(has_children: false, type: 'numbered_list_item') }
 
       it { expect(described_class.permitted_children?(block: block_notion)).to be(false) }
+    end
+
+    it 'returns true for permitted block' do
+      %w[bulleted_list_item numbered_list_item paragraph table].each do |type|
+        hashie_block = Hashie::Mash.new(type: type, has_children: true)
+        expect(described_class.permitted_children?(block: hashie_block)).to be(true)
+      end
     end
   end
 
