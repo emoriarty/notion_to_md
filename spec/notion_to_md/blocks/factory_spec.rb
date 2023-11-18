c6c1d0dcce51bda5f5c1900ca3afa536344c653c
Enrique Arias <emoriarty81@gmail.com>
2023-11-18 22:58:04 +0100

    factory spec

--- spec/notion_to_md/blocks/factory_spec.rb
+++ spec/notion_to_md/blocks/factory_spec.rb
@@ -31,5 +31,61 @@ describe(NotionToMd::Blocks::Factory) do
         expect(output).to be_an_instance_of(NotionToMd::Blocks::TableBlock)
       end
     end
+
+    context 'when using a to_do block' do
+      let(:block_mash) do
+        Hashie::Mash.new(
+          type: 'to_do'
+        )
+      end
+
+      it do
+        output = described_class.build(block: block_mash)
+
+        expect(output).to be_an_instance_of(NotionToMd::Blocks::ToDoRowBlock)
+      end
+    end
+
+    context 'when using a bulleted_list_item block' do
+      let(:block_mash) do
+        Hashie::Mash.new(
+          type: 'bulleted_list_item'
+        )
+      end
+
+      it do
+        output = described_class.build(block: block_mash)
+
+        expect(output).to be_an_instance_of(NotionToMd::Blocks::BulletedListItemBlock)
+      end
+    end
+
+    context 'when using a numbered_list_item block' do
+      let(:block_mash) do
+        Hashie::Mash.new(
+          type: 'numbered_list_item'
+        )
+      end
+
+      it do
+        output = described_class.build(block: block_mash)
+
+        expect(output).to be_an_instance_of(NotionToMd::Blocks::BulletedListItemBlock)
+      end
+    end
+
+    context 'when using a table_row block' do
+      let(:block_mash) do
+        Hashie::Mash.new(
+          type: 'table_row'
+        )
+      end
+
+      it do
+        output = described_class.build(block: block_mash)
+
+        expect(output).to be_an_instance_of(NotionToMd::Blocks::TableRowBlock)
+      end
+    end
   end
 end
