147303c14025bdcc1ffaa43c954320f2c716c0c3
Enrique Arias <emoriarty81@gmail.com>
2023-11-21 14:55:54 +0100

    nest numbered list test

--- spec/notion_to_md/blocks/bulleted_list_block_spec.rb
+++ spec/notion_to_md/blocks/bulleted_list_block_spec.rb
@@ -3,17 +3,18 @@
 require 'spec_helper'
 
 describe(NotionToMd::Blocks::BulletedListBlock) do
+  def create_mash(index)
+     Hashie::Mash.new(
+        type: 'bulleted_list_item',
+        bulleted_list_item: {
+          rich_text: [{ plain_text: "item #{index}", type: 'text', href: nil, annotations: {} }]
+        }
+      )
+  end
+
   describe('#to_md') do
     let(:children) do
-      create_item = lambda do |index|
-        list_item = Hashie::Mash.new(
-          type: 'bulleted_list_item',
-          bulleted_list_item: {
-            rich_text: [{ plain_text: "item #{index}", type: 'text', href: nil, annotations: {} }]
-          }
-        )
-      end
-      3.times.map { |index| NotionToMd::Blocks::BulletedListItemBlock.new(block: create_item.call(index)) }
+      3.times.map { |index| NotionToMd::Blocks::BulletedListItemBlock.new(block: create_mash(index)) }
     end
 
     it do
@@ -24,72 +25,46 @@ describe(NotionToMd::Blocks::BulletedListBlock) do
     end
 
     context 'with a nested list' do
-      let(:parent_list) do
-        raw_list_item = Hashie::Mash.new(
-          type: 'bulleted_list_item',
-          bulleted_list_item: {
-            rich_text: [{ plain_text: 'item A', type: 'text', href: nil, annotations: {} }]
-          }
-        )
+      let(:parent) do
         [
           NotionToMd::Blocks::BulletedListItemBlock.new(
-            block: raw_list_item,
-            children: [NotionToMd::Blocks::BulletedListBlock.new(children: children)]
+            block: create_mash('A'),
+            children: [described_class.new(children: children)]
           )
         ]
       end
 
       it do
-        table = described_class.new(children: parent_list)
+        table = described_class.new(children: parent)
         expected_output = "- item A\n\t- item 0\n\t- item 1\n\t- item 2\n\n"
 
         expect(table.to_md).to eq(expected_output)
       end
     end
 
-    context 'with a second nesting level' do
+    context 'with a second nested level' do
       let(:nested_children) do
-        create_item = lambda do |index|
-          list_item = Hashie::Mash.new(
-            type: 'bulleted_list_item',
-            bulleted_list_item: {
-              rich_text: [{ plain_text: "item #{index}", type: 'text', href: nil, annotations: {} }]
-            }
-          )
-        end
         3.times.map { |index| NotionToMd::Blocks::BulletedListItemBlock.new(
-          block: create_item.call(index * 10),
+          block: create_mash("#{index}0"),
           children: [NotionToMd::Blocks::BulletedListBlock.new(children: children)]
         ) }
       end
-      let(:parent_list) do
-        first_list_item = Hashie::Mash.new(
-          type: 'bulleted_list_item',
-          bulleted_list_item: {
-            rich_text: [{ plain_text: 'item A', type: 'text', href: nil, annotations: {} }]
-          }
-        )
-        second_list_item = Hashie::Mash.new(
-          type: 'bulleted_list_item',
-          bulleted_list_item: {
-            rich_text: [{ plain_text: 'item α', type: 'text', href: nil, annotations: {} }]
-          }
-        )
+      let(:parent) do
         [
           NotionToMd::Blocks::BulletedListItemBlock.new(
-            block: first_list_item,
+            block: create_mash('A'),
             children: [NotionToMd::Blocks::BulletedListBlock.new(children: nested_children)]
           ),
           NotionToMd::Blocks::BulletedListItemBlock.new(
-            block: second_list_item,
+            block: create_mash('α'),
             children: [NotionToMd::Blocks::BulletedListBlock.new(children: nested_children)]
           )
         ]
       end
 
       it do
-        table = described_class.new(children: parent_list)
-        expected_output = "- item A\n\t- item 0\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 10\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 20\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n- item α\n\t- item 0\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 10\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 20\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\n"
+        table = described_class.new(children: parent)
+        expected_output = "- item A\n\t- item 00\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 10\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 20\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n- item α\n\t- item 00\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 10\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\t- item 20\n\t\t- item 0\n\t\t- item 1\n\t\t- item 2\n\n"
 
         expect(table.to_md).to eq(expected_output)
       end
--- spec/notion_to_md/blocks/numbered_list_block_spec.rb
+++ spec/notion_to_md/blocks/numbered_list_block_spec.rb
@@ -3,17 +3,18 @@
 require 'spec_helper'
 
 describe(NotionToMd::Blocks::NumberedListBlock) do
+  def create_mash(index)
+     Hashie::Mash.new(
+        type: 'numbered_list_item',
+        numbered_list_item: {
+          rich_text: [{ plain_text: "item #{index}", type: 'text', href: nil, annotations: {} }]
+        }
+      )
+  end
+
   describe('#to_md') do
     let(:children) do
-      create_item = lambda do |index|
-        list_item = Hashie::Mash.new(
-          type: 'numbered_list_item',
-          numbered_list_item: {
-            rich_text: [{ plain_text: "item #{index}", type: 'text', href: nil, annotations: {} }]
-          }
-        )
-      end
-      3.times.map { |index| NotionToMd::Blocks::NumberedListItemBlock.new(block: create_item.call(index)) }
+      3.times.map { |index| NotionToMd::Blocks::NumberedListItemBlock.new(block: create_mash(index)) }
     end
 
     it do
@@ -24,24 +25,46 @@ describe(NotionToMd::Blocks::NumberedListBlock) do
     end
 
     context 'with a nested list' do
-      let(:parent_list) do
-        raw_list_item = Hashie::Mash.new(
-          type: 'numbered_list_item',
-          numbered_list_item: {
-            rich_text: [{ plain_text: 'item A', type: 'text', href: nil, annotations: {} }]
-          }
-        )
+      let(:parent) do
         [
           NotionToMd::Blocks::NumberedListItemBlock.new(
-            block: raw_list_item,
-            children: [NotionToMd::Blocks::NumberedListBlock.new(children: children)]
+            block: create_mash('A'),
+            children: [described_class.new(children: children)]
           )
         ]
       end
 
       it do
-        table = described_class.new(children: parent_list)
+        table = described_class.new(children: parent)
         expected_output = "1. item A\n\t1. item 0\n\t2. item 1\n\t3. item 2\n\n"
+
+        expect(table.to_md).to eq(expected_output)
+      end
+    end
+
+    context 'with a second nested level' do
+      let(:nested_children) do
+        3.times.map { |index| NotionToMd::Blocks::NumberedListItemBlock.new(
+          block: create_mash("#{index}0"),
+          children: [described_class.new(children: children)]
+        ) }
+      end
+      let(:parent) do
+        [
+          NotionToMd::Blocks::NumberedListItemBlock.new(
+            block: create_mash('A'),
+            children: [described_class.new(children: nested_children)]
+          ),
+          NotionToMd::Blocks::NumberedListItemBlock.new(
+            block: create_mash('α'),
+            children: [described_class.new(children: nested_children)]
+          )
+        ]
+      end
+
+      it do
+        table = described_class.new(children: parent)
+        expected_output = "1. item A\n\t1. item 00\n\t\t1. item 0\n\t\t2. item 1\n\t\t3. item 2\n\t2. item 10\n\t\t1. item 0\n\t\t2. item 1\n\t\t3. item 2\n\t3. item 20\n\t\t1. item 0\n\t\t2. item 1\n\t\t3. item 2\n2. item α\n\t1. item 00\n\t\t1. item 0\n\t\t2. item 1\n\t\t3. item 2\n\t2. item 10\n\t\t1. item 0\n\t\t2. item 1\n\t\t3. item 2\n\t3. item 20\n\t\t1. item 0\n\t\t2. item 1\n\t\t3. item 2\n\n"
 
         expect(table.to_md).to eq(expected_output)
       end
